5a3c52edbbb447abb5e5497c2870ff8931a07f5b
sayole <sylee8191@gmail.com>
2023-03-06 18:10:46 +0900

    [추가] server project 추가

---
+++ udog_server/src/main/java/com/mainproject/udog_server/UdogServerApplication.java
@@ -0,0 +1,13 @@
+package com.mainproject.udog_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdogServerApplication.class, args);
+	}
+
+}
---
+++ udog_server/src/test/java/com/mainproject/udog_server/UdogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainproject.udog_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
