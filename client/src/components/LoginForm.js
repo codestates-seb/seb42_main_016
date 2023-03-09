ca8fa4947199d2a84f23f28f56e736c9b7db1c84
soohyuneee <tlstngus95@naver.com>
2023-03-09 18:07:28 +0900

    [추가] 회원가입, 로그인 추가 (2023.03.09 신수현)

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,98 @@
+import * as S from './style/FormStyle';
+import {useRef, useState} from 'react';
+import useEmail from '../hooks/useEmail';
+import usePassword from '../hooks/usePassword';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+
+function LoginForm() {
+	const [value, setValue] = useState({
+		email: '',
+		password: '',
+	});
+
+	const {email, password} = value;
+	const {validEmailText, isValidEmail} = useEmail(email);
+	const {validPasswordText, isValidPassword} = usePassword(password);
+	const inputRef = useRef([]);
+	const navigate = useNavigate();
+
+	const onChange = (e) => {
+		const changeValue = {
+			...value,
+			[e.target.name]: e.target.value,
+		};
+		setValue(changeValue);
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		if (!email.length || isValidEmail) {
+			inputRef.current[0].focus();
+			return;
+		}
+		if (!password.length || isValidPassword) {
+			inputRef.current[1].focus();
+			return;
+		}
+		axios
+			.post('/auth/login', {
+				email,
+				password,
+			})
+			.then((res) => {
+				console.log('로그인 성공!', res.data);
+				localStorage.setItem('accessToken', res.headers.authorization);
+				localStorage.setItem('username', res.data.username);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<S.BgColor>
+			<S.Container>
+				<S.Border>
+					<S.FormWrapper>
+						<S.Logo>UDog</S.Logo>
+						<S.Form>
+							<S.LabelContainer>
+								<S.Label>이메일</S.Label>
+							</S.LabelContainer>
+							<S.Input
+								type="email"
+								placeholder="이메일을 입력해주세요."
+								name="email"
+								value={email}
+								onChange={onChange}
+								valid={!isValidEmail}
+								ref={(el) => (inputRef.current[0] = el)}
+							/>
+							<S.ValidText>{validEmailText}</S.ValidText>
+							<S.LabelContainer>
+								<S.Label>비밀번호</S.Label>
+							</S.LabelContainer>
+							<S.Input
+								type="password"
+								placeholder="비밀번호를 입력해주세요."
+								name="password"
+								value={password}
+								onChange={onChange}
+								valid={!isValidPassword}
+								ref={(el) => (inputRef.current[1] = el)}
+							/>
+							<S.ValidText>{validPasswordText}</S.ValidText>
+							<S.Button type="submit" onClick={onSubmit}>
+								<S.ButtonText>로그인</S.ButtonText>
+							</S.Button>
+							<S.IsMember onClick={() => navigate('/signup')}>회원이 아니신가요?</S.IsMember>
+						</S.Form>
+					</S.FormWrapper>
+				</S.Border>
+			</S.Container>
+		</S.BgColor>
+	);
+}
+
+export default LoginForm;
---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,134 @@
+import * as S from './style/FormStyle';
+import {useRef, useState} from 'react';
+import useNickname from '../hooks/useNickname';
+import useEmail from '../hooks/useEmail';
+import usePassword from '../hooks/usePassword';
+import useConfirmPassword from '../hooks/useConfirmPassword';
+import axios from 'axios';
+
+function SignupForm() {
+	const [value, setValue] = useState({
+		nickname: '',
+		email: '',
+		password: '',
+		confirmPassword: '',
+	});
+
+	const {nickname, email, password, confirmPassword} = value;
+	const {validNicknameText, isValidNickname} = useNickname(nickname);
+	const {validEmailText, isValidEmail} = useEmail(email);
+	const {validPasswordText, isValidPassword} = usePassword(password);
+	const {validConfirmPasswordText, isValidConfirmPassword} = useConfirmPassword(confirmPassword, password);
+	const inputRef = useRef([]);
+
+	const onChange = (e) => {
+		const changeValue = {
+			...value,
+			[e.target.name]: e.target.value,
+		};
+		setValue(changeValue);
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		if (isValidNickname) {
+			inputRef.current[0].focus();
+			return;
+		}
+		if (isValidEmail) {
+			inputRef.current[1].focus();
+			return;
+		}
+		if (isValidPassword) {
+			inputRef.current[2].focus();
+			return;
+		}
+		if (isValidConfirmPassword) {
+			inputRef.current[3].focus();
+			return;
+		}
+		axios
+			.post('/members/sign-up', {
+				nickname,
+				email,
+				password,
+			})
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<S.BgColor>
+			<S.Container>
+				<S.Border>
+					<S.FormWrapper>
+						<S.Logo>UDog</S.Logo>
+						<S.Form>
+							<S.LabelContainer>
+								<S.Label>닉네임</S.Label>
+							</S.LabelContainer>
+							<S.Input
+								type="text"
+								placeholder="닉네임을 입력해주세요."
+								name="nickname"
+								value={nickname}
+								onChange={onChange}
+								valid={!isValidNickname}
+								ref={(el) => (inputRef.current[0] = el)}
+							/>
+							<S.ValidText>{validNicknameText}</S.ValidText>
+							<S.LabelContainer>
+								<S.Label>이메일</S.Label>
+							</S.LabelContainer>
+							<S.Input
+								type="email"
+								placeholder="이메일을 입력해주세요."
+								name="email"
+								value={email}
+								onChange={onChange}
+								valid={!isValidEmail}
+								ref={(el) => (inputRef.current[1] = el)}
+							/>
+							<S.ValidText>{validEmailText}</S.ValidText>
+							<S.LabelContainer>
+								<S.Label>비밀번호</S.Label>
+							</S.LabelContainer>
+							<S.Input
+								type="password"
+								placeholder="비밀번호를 입력해주세요."
+								name="password"
+								value={password}
+								onChange={onChange}
+								valid={!isValidPassword}
+								ref={(el) => (inputRef.current[2] = el)}
+							/>
+							<S.ValidText>{validPasswordText}</S.ValidText>
+							<S.LabelContainer>
+								<S.Label>비밀번호 확인</S.Label>
+							</S.LabelContainer>
+							<S.Input
+								type="password"
+								placeholder="비밀번호를 다시 입력해주세요."
+								name="confirmPassword"
+								value={confirmPassword}
+								onChange={onChange}
+								valid={!isValidConfirmPassword}
+								ref={(el) => (inputRef.current[3] = el)}
+							/>
+							<S.ValidText>{validConfirmPasswordText}</S.ValidText>
+							<S.Button type="submit" onClick={onSubmit} disabled={!(nickname.length && email.length && password.length && confirmPassword.length)}>
+								<S.ButtonText>가입하기</S.ButtonText>
+							</S.Button>
+						</S.Form>
+					</S.FormWrapper>
+				</S.Border>
+			</S.Container>
+		</S.BgColor>
+	);
+}
+
+export default SignupForm;
