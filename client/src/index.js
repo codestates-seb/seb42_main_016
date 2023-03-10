2ca00454b63e735d888cc0f463c37cebf67dce9d
soohyuneee <tlstngus95@naver.com>
2023-03-10 14:07:52 +0900

    [추가] 리덕스 스토어 설정 추가 (2023.03.10 신수현)

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import store from './modules/redux/store';
+import {Provider} from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
