7596026783120ba6a685bd24498ff5ee619e7774
soohyuneee <tlstngus95@naver.com>
2023-03-12 03:59:47 +0900

    [추가] axios 커스텀훅 추가 (2023.03.12 신수현)

---
+++ client/src/hooks/useAxios.js
@@ -0,0 +1,59 @@
+import API from '../modules/API';
+import {useNavigate} from 'react-router-dom';
+
+const useAxios = (url, body, path) => {
+	const token = localStorage.getItem('accessToken');
+	const refresh = localStorage.getItem('refresh');
+	const navigate = useNavigate();
+
+	const POST = (url, body, path) => {
+		API.post(url, body, {
+			headers: {
+				Authorization: token,
+				Refresh: refresh,
+			},
+		})
+			.then((res) => {
+				console.log('등록성공', res);
+				navigate(path);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	const PATCH = (url, body, path) => {
+		API.patch(url, body, {
+			headers: {
+				Authorization: token,
+				Refresh: refresh,
+			},
+		})
+			.then((res) => {
+				console.log('수정성공', res);
+				navigate(path);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	const DELETE = (url) => {
+		API.delete(url, {
+			headers: {
+				Authorization: token,
+				Refresh: refresh,
+			},
+		})
+			.then((res) => {
+				console.log('삭제성공', res);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return {POST, PATCH, DELETE};
+};
+
+export default useAxios;
