2cbf9438327c4fd5146a238c1b654cc3f913d011
soohyuneee <tlstngus95@naver.com>
2023-03-11 11:24:06 +0900

    [수정] 스토어 수정 (2023.03.11 신수현)

--- client/src/modules/redux/store.js
+++ client/src/modules/redux/store.js
@@ -1,9 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import userSlice from './userSlice';
+import emptySlice from './emptySlice';
+import modalSlice from './modalSlice';
 
 const store = configureStore({
 	reducer: {
 		user: userSlice,
+		empty: emptySlice,
+		modal: modalSlice,
 	},
 });
 
