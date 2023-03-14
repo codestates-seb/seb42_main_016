749b5af4fd9bb9118ac2c05dca43e01abf820510
soohyuneee <tlstngus95@naver.com>
2023-03-14 22:22:33 +0900

    [수정]  로딩 상태 업데이트 반영 (2023.03.14 신수현)

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,14 +1,28 @@
 import {useState, useEffect} from 'react';
 import API from '../modules/API';
+import {useDispatch} from 'react-redux';
+import {setLoading} from '../modules/redux/loadingSlice';
 
 function useFetch(url) {
+	const dispatch = useDispatch();
 	const [data, setData] = useState([]);
+
 	useEffect(() => {
 		if (window) window.scrollTo(0, 0);
-		API.get(url).then((res) => {
-			setData(res.data);
-		});
-	}, [url]);
+		dispatch(setLoading(true));
+
+		API.get(url)
+			.then((res) => {
+				setData(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+			.finally(() => {
+				dispatch(setLoading(false));
+			});
+	}, [url, dispatch]);
+
 	return data;
 }
 
