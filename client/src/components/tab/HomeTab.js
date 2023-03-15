c6d727dcfac0396a4dc0d939d28cfac8db580e05
soohyuneee <tlstngus95@naver.com>
2023-03-15 23:04:27 +0900

    [추가] 미용실 좋아요 추가 (2023.03.15 신수현)

--- client/src/components/tab/HomeTab.js
+++ client/src/components/tab/HomeTab.js
@@ -1,40 +1,25 @@
 import * as S from '../style/HomeStyle';
 import img from '../../utils/img.jpeg';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {selectLoading} from '../../modules/redux/loadingSlice';
 import Loading from '../Loading';
 import ClockIcon from '../../utils/ClockIcon';
 import PhoneIcon from '../../utils/PhoneIcon';
 import ReviewIcon from '../../utils/ReviewIcon';
 import {IoHeartOutline, IoHeart} from 'react-icons/io5';
-import {selectUser} from '../../modules/redux/userSlice';
-import {openModal} from '../../modules/redux/modalSlice';
-import {selectLike, setLike} from '../../modules/redux/likeSlice';
+import {selectLike} from '../../modules/redux/likeSlice';
 import useComment from '../../hooks/useComment';
+import useShopLike from '../../hooks/useShopLike';
 
 function HomeTab({data}) {
 	const {loading} = useSelector(selectLoading);
-	const {user} = useSelector(selectUser);
 	const {like} = useSelector(selectLike);
-	const dispatch = useDispatch();
 	const {show, handleToggle, comment, getDisplayComment} = useComment(data.hairShopDescription);
+	const {onLikeButtonClick} = useShopLike(data.hairShopId);
 
 	if (loading) {
 		return <Loading />;
 	}
-
-	const onLikeButtonClick = () => {
-		if (!user) {
-			dispatch(
-				openModal({
-					modalType: 'IsLoginModal',
-					isOpen: true,
-				})
-			);
-			return;
-		}
-		dispatch(setLike(!like));
-	};
 
 	return (
 		<S.HomeContainer>
---
+++ client/src/hooks/useShopLike.js
@@ -0,0 +1,75 @@
+import {useSelector, useDispatch} from 'react-redux';
+import {openModal} from '../modules/redux/modalSlice';
+import {selectUser} from '../modules/redux/userSlice';
+import {selectLike, setLike, setIsSubmit} from '../modules/redux/likeSlice';
+import {addLike, cancleLike} from '../modules/redux/shopSlice';
+import {useState} from 'react';
+import API from '../modules/API';
+
+function useShopLike(id) {
+	const {user} = useSelector(selectUser);
+	const {like, isSubmit} = useSelector(selectLike);
+	const dispatch = useDispatch();
+	const token = localStorage.getItem('accessToken');
+	const refresh = localStorage.getItem('refresh');
+	const [likeId, setLikeId] = useState(0);
+
+	const onLikeButtonClick = () => {
+		if (!user) {
+			dispatch(
+				openModal({
+					modalType: 'IsLoginModal',
+					isOpen: true,
+				})
+			);
+			return;
+		}
+
+		if (isSubmit) {
+			return;
+		}
+
+		dispatch(setIsSubmit(true));
+
+		if (!like) {
+			API.post(
+				`/hair-shop-likes/${id}/likes`,
+				{hairShopId: id},
+				{
+					headers: {
+						Authorization: token,
+						Refresh: refresh,
+					},
+				}
+			)
+				.then((res) => {
+					console.log('좋아요 등록', res);
+					setLikeId(res.data.hairShopLikeId);
+					dispatch(setLike(true));
+					dispatch(addLike());
+				})
+				.finally(() => {
+					dispatch(setIsSubmit(false));
+				});
+		} else {
+			API.post(`/hair-shop-likes/${id}/${likeId}/dislikes`, {
+				headers: {
+					Authorization: token,
+					Refresh: refresh,
+				},
+			})
+				.then((res) => {
+					console.log('좋아요 취소', res);
+					dispatch(setLike(false));
+					dispatch(cancleLike());
+				})
+				.finally(() => {
+					dispatch(setIsSubmit(false));
+				});
+		}
+	};
+
+	return {onLikeButtonClick};
+}
+
+export default useShopLike;
