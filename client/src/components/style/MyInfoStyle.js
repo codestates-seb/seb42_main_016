f7a21ea9abf16a857de205bc05f557d2ed757564
soohyuneee <tlstngus95@naver.com>
2023-03-16 16:15:30 +0900

    [추가] 마이페이지 추가 (2023.03.16 신수현)

--- client/src/components/style/MyInfoStyle.js
+++ client/src/components/style/MyInfoStyle.js
@@ -30,9 +30,75 @@ export const InfoButton = styled.button`
 	font-size: 14px;
 	background-color: transparent;
 	border: none;
-	color: #8c949c;
+	color: #666;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	svg {
-		font-size: 16px;
+		font-size: 18px;
 		vertical-align: middle;
+		margin-bottom: 2px;
+	}
+`;
+
+export const ContentTitleContainer = styled.div`
+	padding-top: 25px;
+	overflow: hidden;
+	position: relative;
+	width: 100%;
+	padding: 36px 0 7px;
+	border-bottom: 1px solid #666;
+	font-size: 0;
+	line-height: 0;
+`;
+
+export const ContentTitle = styled.h2`
+	float: left;
+	color: #333;
+	font-size: 20px;
+	line-height: 30px;
+	margin: 0;
+`;
+
+export const MoreButton = styled.button`
+	position: absolute;
+	background-color: transparent;
+	border: none;
+	bottom: 10px;
+	right: 0;
+	padding: 0 15px 0 0;
+	color: #666;
+	font-size: 14px;
+	line-height: 20px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	svg {
+		font-size: 18px;
+		vertical-align: middle;
+	}
+`;
+
+export const MyInfoContent = styled.div`
+	border-bottom: 1px solid #ddd;
+	height: 300px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const WithdrawText = styled.div`
+	width: 100%;
+	height: 100px;
+	margin-top: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: #666;
+	font-size: 14px;
+	cursor: pointer;
+	span {
+		border-bottom: 1px solid #666;
 	}
 `;
