fe17e220a6307768cabfbbf62a67adc180358efc
sayole <sylee8191@gmail.com>
2023-03-16 16:08:15 +0900

    [추가] member composite service 적용 완료(2023.03.16)

--- udog_server/src/main/java/com/mainproject/udog_server/api/composite_service/MemberCompositeService.java
+++ udog_server/src/main/java/com/mainproject/udog_server/api/composite_service/MemberCompositeService.java
@@ -1,4 +1,35 @@
 package com.mainproject.udog_server.api.composite_service;
 
+import com.mainproject.udog_server.member.*;
+import lombok.*;
+
+@RequiredArgsConstructor
 public class MemberCompositeService {
+    private final MemberService memberService;
+
+    public Member createMember(Member member){
+        return memberService.createMember(member);
+    }
+
+    public Member updateMemberNickname(Member member, String email){
+        Member foundMember = memberService.findLoginMemberByEmail(email);
+        member.setMemberId(foundMember.getMemberId());
+        return memberService.updateMemberNickname(member);
+    }
+
+    public Member updateMemberPassword(Member member, String prevPassword, String email){
+        Member foundMember = memberService.findLoginMemberByEmail(email);
+        memberService.verifyPasswordMatch(member, prevPassword);
+        member.setMemberId(foundMember.getMemberId());
+        return memberService.updateMemberPassword(member);
+    }
+
+    public Member findMember(String email){
+        return memberService.findLoginMemberByEmail(email);
+    }
+
+    public void deleteMember(String email){
+        Member foundMember = memberService.findLoginMemberByEmail(email);
+        memberService.deleteMember(foundMember);
+    }
 }
--- udog_server/src/main/java/com/mainproject/udog_server/api/controller/MemberController.java
+++ udog_server/src/main/java/com/mainproject/udog_server/api/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.mainproject.udog_server.api.controller;
 
+import com.mainproject.udog_server.api.composite_service.*;
 import com.mainproject.udog_server.api.dto.*;
 import com.mainproject.udog_server.api.mapper.*;
 import com.mainproject.udog_server.member.*;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,39 +12,33 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.*;
 import java.security.Principal;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/members")
+@RequiredArgsConstructor
 @Validated
 @Slf4j
 public class MemberController {
-    private final MemberService memberService;
+    private final static String MEMBER_DEFAULT_URL = "/members";
+    private final MemberCompositeService compositeService;
     private final MemberMapper mapper;
-
-    public MemberController(MemberMapper mapper, MemberService memberService){
-        this.memberService = memberService;
-        this.mapper = mapper;
-    }
 
     @PostMapping
     public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post memberPost){
         Member member = mapper.MemberPostToMember(memberPost);
 
-        Member createdMember = memberService.createMember(member);
-
-        return new ResponseEntity<>(mapper.MemberToMemberResponseDto(createdMember), HttpStatus.CREATED);
+        Member createdMember = compositeService.createMember(member);
+        return ResponseEntity.status(HttpStatus.CREATED).location(URI.create(MEMBER_DEFAULT_URL)).build();
     }
 
     @PatchMapping("nickname")
     public ResponseEntity patchMemberNickname(Principal principal,
                                       @Valid @RequestBody MemberDto.NicknamePatch memberPatch){
-        long loginMemberId = memberService.findLoginMemberByEmail(principal.getName()).getMemberId();
-        memberPatch.setMemberId(loginMemberId);
-
         Member member = mapper.MemberNicknamePatchDtoToMember(memberPatch);
-        Member updateMember = memberService.updateMemberNickname(member);
+        Member updateMember = compositeService.updateMemberNickname(member, principal.getName());
 
         return new ResponseEntity<>(mapper.MemberToMemberResponseDto(updateMember), HttpStatus.OK);
     }
@@ -50,29 +46,21 @@ public class MemberController {
     @PatchMapping("password")
     public ResponseEntity patchMemberPassword(Principal principal,
                                       @Valid @RequestBody MemberDto.PasswordPatch memberPatch){
-        long loginMemberId = memberService.findLoginMemberByEmail(principal.getName()).getMemberId();
-        memberPatch.setMemberId(loginMemberId);
-        memberService.verifyPasswordMatch(loginMemberId, memberPatch.getPrevPassword());
-
         Member member = mapper.MemberPasswordPatchDtoToMember(memberPatch);
-        Member updateMember = memberService.updateMemberPassword(member);
+        Member updateMember = compositeService.updateMemberPassword(member, memberPatch.getPrevPassword(), principal.getName());
 
         return new ResponseEntity<>(mapper.MemberToMemberResponseDto(updateMember), HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity getMember(Principal principal){
-        long loginMemberId = memberService.findLoginMemberByEmail(principal.getName()).getMemberId();
-
-        Member response = memberService.findMember(loginMemberId);
+        Member response = compositeService.findMember(principal.getName());
         return new ResponseEntity<>(mapper.MemberToMemberResponseDto(response), HttpStatus.OK);
     }
 
     @DeleteMapping
     public ResponseEntity deleteMember(Principal principal) {
-        long loginMemberId = memberService.findLoginMemberByEmail(principal.getName()).getMemberId();
-
-        memberService.deleteMember(loginMemberId);
+        compositeService.deleteMember(principal.getName());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
