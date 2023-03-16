8345262ef7f321c613a47dd6f8cbf238b8fde5c4
kim changha <ckdgk0366@gmail.com>
2023-03-17 04:33:22 +0900

    [추가] ReviewController @CrossOrigin 어노테이션 추가 (2023.03.17 김창하)

--- udog_server/src/main/java/com/mainproject/udog_server/api/controller/ReviewController.java
+++ udog_server/src/main/java/com/mainproject/udog_server/api/controller/ReviewController.java
@@ -9,15 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Positive;
 import java.net.URI;
@@ -27,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/reviews")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ReviewController {
 
     private final ReviewCompositeService compositeService;
