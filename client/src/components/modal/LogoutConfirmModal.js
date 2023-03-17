f399553962da91aadb8cf85ddb987652d221f4ed
soohyuneee <tlstngus95@naver.com>
2023-03-17 17:36:42 +0900

    [수정] eslint 주석 (2023.03.17 신수현)

--- client/src/components/modal/LogoutConfirmModal.js
+++ client/src/components/modal/LogoutConfirmModal.js
@@ -1,58 +1,54 @@
 import * as S from '../style/ModalStyle';
-import { useRef, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectModal, closeModal } from '../../modules/redux/modalSlice';
+import {useRef, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {selectModal, closeModal} from '../../modules/redux/modalSlice';
 import useScroll from '../../hooks/useScroll';
 
 function LogoutConfirmModal() {
-  const modalRef = useRef();
-  const { isOpen } = useSelector(selectModal);
-  const dispatch = useDispatch();
+	const modalRef = useRef();
+	const {isOpen} = useSelector(selectModal);
+	const dispatch = useDispatch();
 
-  useScroll();
+	useScroll();
 
-  useEffect(() => {
-    document.addEventListener('mousedown', clickModalOutside);
-    return () => {
-      document.removeEventListener('mousedown', clickModalOutside);
-    };
-  });
+	useEffect(() => {
+		document.addEventListener('mousedown', clickModalOutside);
+		return () => {
+			document.removeEventListener('mousedown', clickModalOutside);
+		};
+	});
 
-  const clickModalOutside = (e) => {
-    if (isOpen && !modalRef.current.contains(e.target)) {
-      dispatch(closeModal());
-    }
-  };
+	const clickModalOutside = (e) => {
+		if (isOpen && !modalRef.current.contains(e.target)) {
+			dispatch(closeModal());
+		}
+	};
 
-  const clickCancle = () => {
-    dispatch(closeModal());
-  };
+	const clickCancle = () => {
+		dispatch(closeModal());
+	};
 
-  const onClickDelete = () => {
-    localStorage.clear();
-    location.reload();
-  };
+	const onClickDelete = () => {
+		localStorage.clear();
+		// eslint-disable-next-line
+		location.reload();
+	};
 
-  return (
-    <S.ConfirmContainer ref={modalRef}>
-      <S.ConfirmContent>
-        <S.ConfirmText>정말로 로그아웃하시겠습니까?</S.ConfirmText>
-        <S.ButtonBox>
-          <S.ConfirmButton bgcolor="white" onClick={clickCancle}>
-            취소
-          </S.ConfirmButton>
-          <S.ConfirmButton
-            color="white"
-            border="none"
-            hover="#6893dd"
-            onClick={onClickDelete}
-          >
-            로그아웃
-          </S.ConfirmButton>
-        </S.ButtonBox>
-      </S.ConfirmContent>
-    </S.ConfirmContainer>
-  );
+	return (
+		<S.ConfirmContainer ref={modalRef}>
+			<S.ConfirmContent>
+				<S.ConfirmText>정말로 로그아웃하시겠습니까?</S.ConfirmText>
+				<S.ButtonBox>
+					<S.ConfirmButton bgcolor="white" onClick={clickCancle}>
+						취소
+					</S.ConfirmButton>
+					<S.ConfirmButton color="white" border="none" hover="#6893dd" onClick={onClickDelete}>
+						로그아웃
+					</S.ConfirmButton>
+				</S.ButtonBox>
+			</S.ConfirmContent>
+		</S.ConfirmContainer>
+	);
 }
 
 export default LogoutConfirmModal;
