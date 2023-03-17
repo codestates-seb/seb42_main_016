9fedbb9e0b7da59fc4d6aeb7434462f2e0b82b08
soohyuneee <tlstngus95@naver.com>
2023-03-17 15:21:51 +0900

    [추가] 로그아웃 기능 추가 (2023.03.17 신수현)

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,30 @@
 import * as S from './style/HeaderStyle';
-import { HOME, LOGIN, STYLEBOOK, SIGNUP, HAIRSHOP } from '../modules/routes';
+import {
+  HOME,
+  LOGIN,
+  STYLEBOOK,
+  SIGNUP,
+  HAIRSHOP,
+  MYPAGE,
+} from '../modules/routes';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectUser } from '../modules/redux/userSlice';
+import { openModal } from '../modules/redux/modalSlice';
+import { LOGOUTCONFIRMMODAL } from '../modules/ModalContainer';
 
 function Header() {
+  const { user } = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const onClickLogout = () => {
+    dispatch(
+      openModal({
+        modalType: LOGOUTCONFIRMMODAL,
+        isOpen: true,
+      })
+    );
+  };
+
   return (
     <S.Container>
       <S.NavStyle>
@@ -24,10 +47,18 @@ function Header() {
         </S.Menu>
         <S.Login>
           <S.List>
-            <S.LinkStyle to={LOGIN}>로그인</S.LinkStyle>
+            {user ? (
+              <S.LinkStyle onClick={onClickLogout}>로그아웃</S.LinkStyle>
+            ) : (
+              <S.LinkStyle to={LOGIN}>로그인</S.LinkStyle>
+            )}
           </S.List>
           <S.List>
-            <S.LinkStyle to={SIGNUP}>회원가입</S.LinkStyle>
+            {user ? (
+              <S.NavLinkStyle to={MYPAGE}>마이페이지</S.NavLinkStyle>
+            ) : (
+              <S.LinkStyle to={SIGNUP}>회원가입</S.LinkStyle>
+            )}
           </S.List>
         </S.Login>
       </S.NavStyle>
