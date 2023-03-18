90a87595444826a0bdc8b9bf728fe37dd6458179
sayole <sylee8191@gmail.com>
2023-03-19 04:58:37 +0900

    [추가] styleBook GET시response에 내가 좋아요 누른 게시물의 styleLikeId 담기(2023.03.19 이상윤)

--- udog_server/src/main/java/com/mainproject/udog_server/api/mapper/StyleBookMapper.java
+++ udog_server/src/main/java/com/mainproject/udog_server/api/mapper/StyleBookMapper.java
@@ -10,6 +10,7 @@ import java.util.*;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface StyleBookMapper {
     @Mapping(source = "hairShop.hairShopId", target = "hairShopId")
+    @Mapping(source = "myStyleLikeId", target = "styleLikeId")
     StyleBookResponseDto reviewToStyleBookResponse(Review review);
     List<StyleBookResponseDto> reviewsToStyleBookResponses(List<Review> reviews);
 }
--- udog_server/src/main/java/com/mainproject/udog_server/review/Review.java
+++ udog_server/src/main/java/com/mainproject/udog_server/review/Review.java
@@ -45,4 +45,7 @@ public class Review {
 
     @Transient
     private int likeCount;
+
+    @Transient
+    private long myStyleLikeId;
 }
