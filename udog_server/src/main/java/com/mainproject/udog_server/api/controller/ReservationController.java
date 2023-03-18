922b500f0d7d49538185dcca33ec0b676e8841ec
kim changha <ckdgk0366@gmail.com>
2023-03-18 18:50:32 +0900

    [추가] ReservationController 필요한 CRUD 뼈대 구성 (2023.03.18 김창하)

--- udog_server/src/main/java/com/mainproject/udog_server/api/controller/ReservationController.java
+++ udog_server/src/main/java/com/mainproject/udog_server/api/controller/ReservationController.java
@@ -1,4 +1,36 @@
 package com.mainproject.udog_server.api.controller;
 
+import com.mainproject.udog_server.api.composite_service.ReservationCompositeService;
+import com.mainproject.udog_server.api.mapper.ReservationMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Positive;
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/reservations")
+@RequiredArgsConstructor
+@CrossOrigin
 public class ReservationController {
+    private final ReservationCompositeService compositeService;
+    private final ReservationMapper mapper;
+
+    @PostMapping
+    public ResponseEntity postReservation() {
+        return null;
+    }
+
+    // TODO: 특정 예약 정보 가져오기 필요한지?
+
+    @GetMapping
+    public ResponseEntity getReservations() {
+        return null;
+    }
+
+    @DeleteMapping("{reservation-id}")
+    public ResponseEntity deleteReservation(@PathVariable("reservation-id") @Positive Long reservationId, Principal principal) {
+        return null;
+    }
 }
