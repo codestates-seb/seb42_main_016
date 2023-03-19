3123e79dfd1e2f242c13b189ab7661d34acf7920
sayole <sylee8191@gmail.com>
2023-03-19 17:21:11 +0900

    [추가] 스타일북 Top3 JPQL 추가(2023.03.19 이상윤)

--- udog_server/src/main/java/com/mainproject/udog_server/styleBook/StyleBookRepository.java
+++ udog_server/src/main/java/com/mainproject/udog_server/styleBook/StyleBookRepository.java
@@ -3,6 +3,12 @@ package com.mainproject.udog_server.styleBook;
 import com.mainproject.udog_server.review.*;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.*;
+import java.util.*;
 
 public interface StyleBookRepository extends JpaRepository<Review, Long> {
+    @Query("Select r from Review r order by r.styleLikes.size desc")
+    List<Review> findTop(Pageable pageable);
 }
--- udog_server/src/main/java/com/mainproject/udog_server/styleBook/StyleBookService.java
+++ udog_server/src/main/java/com/mainproject/udog_server/styleBook/StyleBookService.java
@@ -6,17 +6,23 @@ import lombok.*;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.*;
 
+import javax.persistence.*;
 import java.util.*;
 import java.util.stream.*;
 
 @Service
 @RequiredArgsConstructor
 public class StyleBookService {
+    private static final int AMOUNT_OF_TOP = 3;
     private final StyleBookRepository styleBookRepository;
-
-    private final StyleLikeRepository styleLikeRepository;
 
     public Page<Review> findStyles(int page, int size) {
         return styleBookRepository.findAll(PageRequest.of(page, size, Sort.by("createdAt").descending()));
+    }
+
+    public void test(){
+        List<Review> topThree = styleBookRepository.findTop(PageRequest.of(0,AMOUNT_OF_TOP));
+        topThree.forEach(e -> System.out.println(e.getReviewId()));
+
     }
 }
