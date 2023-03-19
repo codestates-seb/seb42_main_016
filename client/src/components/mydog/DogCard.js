9e749ae7de57bb4e7c69fb483d8c571d900d4606
soohyuneee <tlstngus95@naver.com>
2023-03-20 01:16:27 +0900

    [추가] 강아지 나이구하기 유틸함수 작성 (2023.03.20 신수현)

--- client/src/components/mydog/DogCard.js
+++ client/src/components/mydog/DogCard.js
@@ -7,9 +7,9 @@ import { setUrl, setId } from '../../modules/redux/setSlice';
 import { MYDOGEDIT } from '../../modules/routes';
 import { MYDOG_ENDPOINT } from '../../modules/endpoints';
 import { DOGCONFIRMMODAL } from '../../modules/ModalContainer';
+import { getDogAge } from '../../utils/getDogAge';
 
 function DogCard({ dog }) {
-  const age = `${new Date().getFullYear() - dog.dogBirthDate.slice(0, 4)}살`;
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const originType = dog.dogSpecies.replace(/_/g, ' ');
@@ -34,7 +34,7 @@ function DogCard({ dog }) {
     <S.Container>
       <S.TextWrapper>
         <S.Name>{dog.dogName}</S.Name>
-        <S.Detail>{age}</S.Detail>
+        <S.Detail>{getDogAge(dog.dogBirthDate)}</S.Detail>
         <S.Detail>{dog.dogWeight}</S.Detail>
         <S.Detail>{originType}</S.Detail>
         <S.Detail>{dog.dogDescription ? dog.dogDescription : '특이사항 없음'}</S.Detail>
