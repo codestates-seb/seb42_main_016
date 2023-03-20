397be799a0b405f41cb78ce9395f1eb0a9b128d4
soohyuneee <tlstngus95@naver.com>
2023-03-20 14:19:45 +0900

    [추가] 베스트 컨텐츠 연결 (2023.03.20 신수현)

--- client/src/components/mainpage/BestStyleList.js
+++ client/src/components/mainpage/BestStyleList.js
@@ -1,21 +1,21 @@
 import * as S from '../style/SBStyle';
 import StylebookItem from '../stylebook/StylebookItem';
-import useInfiniteScroll from '../../hooks/useInfiniteScroll';
 import { STYLEBOOK_ENDPOINT } from '../../modules/endpoints';
+import useFetch from '../../hooks/useFetch';
 
 export default function BestStyleList() {
-  const PER_PAGE = 3;
-  const { data } = useInfiniteScroll(STYLEBOOK_ENDPOINT, PER_PAGE);
+  const data = useFetch(`${STYLEBOOK_ENDPOINT}/top`);
 
   return (
     <S.Container>
-      {data.map((style) => (
-        <StylebookItem
-          key={style.reviewId}
-          style={style}
-          src={`https://source.unsplash.com/random/${style.reviewId}`}
-        />
-      ))}
+      {data &&
+        data.map((style) => (
+          <StylebookItem
+            key={style.reviewId}
+            style={style}
+            src={`https://source.unsplash.com/random/${style.reviewId}`}
+          />
+        ))}
     </S.Container>
   );
 }
