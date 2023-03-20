ed885895d214f9453a3ca889223b1054eba548b1
soohyuneee <tlstngus95@naver.com>
2023-03-21 03:38:02 +0900

    [추가] 마이페이지 navigate추가 (2023.03.20 신수현)

--- client/src/pages/Mypage.js
+++ client/src/pages/Mypage.js
@@ -1,15 +1,21 @@
 import MypageSidebar from '../components/mypage/MypageSidebar';
 import * as S from '../components/style/MyPageStyle';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 import Header from '../components/Header';
 import { Footer } from '../components/style/FooterStyle';
 import TopButton from '../components/stylebook/TopButton';
 import { useSelector } from 'react-redux';
 import { selectLoading } from '../modules/redux/loadingSlice';
 import Loading from '../components/Loading';
+import { selectUser } from '../modules/redux/userSlice';
 
 export default function Mypage() {
   const { loading } = useSelector(selectLoading);
+  const { user } = useSelector(selectUser);
+
+  if (!user) {
+    return <Navigate replace to="/login" />;
+  }
 
   return (
     <S.MyPageWrapper>
