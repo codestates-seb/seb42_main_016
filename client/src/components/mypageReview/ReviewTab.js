bfc48c9e2a84a32482178ddccb706228cce33a08
blueberryade <blueberryade7@gmail.com>
2023-03-22 11:38:11 +0900

    [수정]마이페이지 리뷰탭 css수정(2023.03.22 조정인)

--- client/src/components/mypageReview/ReviewTab.js
+++ client/src/components/mypageReview/ReviewTab.js
@@ -4,10 +4,14 @@ import { WRITEREVIEW, READREVIEW } from '../../modules/routes';
 export default function ReviewTab() {
   return (
     <S.TabWrap>
-      <NavLink to={WRITEREVIEW}>
+      <NavLink
+        to={WRITEREVIEW}
+        className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
         <div>작성 가능한 리뷰</div>
       </NavLink>
-      <NavLink to={READREVIEW}>
+      <NavLink
+        to={READREVIEW}
+        className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
         <div>작성한 리뷰</div>
       </NavLink>
     </S.TabWrap>
