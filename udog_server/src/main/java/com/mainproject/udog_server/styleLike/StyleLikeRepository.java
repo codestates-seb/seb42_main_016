081804934aa497d88d1b19abbb667105ae7c1567
sayole <sylee8191@gmail.com>
2023-03-24 15:15:14 +0900

    [수정] 스타일 top3 지난 24시간 기준으로 구현(2023.03.24 이상윤)

--- udog_server/src/main/java/com/mainproject/udog_server/styleLike/StyleLikeRepository.java
+++ udog_server/src/main/java/com/mainproject/udog_server/styleLike/StyleLikeRepository.java
@@ -1,12 +1,37 @@
 package com.mainproject.udog_server.styleLike;
 
+import com.mainproject.udog_server.hairshop.*;
 import com.mainproject.udog_server.member.*;
 import com.mainproject.udog_server.review.*;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.*;
 
+import java.time.*;
 import java.util.*;
 
 public interface StyleLikeRepository extends JpaRepository<StyleLike, Long> {
     int countByReviewReviewId(Long reviewId);
     Optional<StyleLike> findByMemberAndReview(Member member, Review review);
+    @Query("SELECT sl.review, count(sl.review) FROM StyleLike sl WHERE sl.createdAt > :beginOfParam GROUP BY sl.review ORDER BY count(sl.review) DESC")
+    List<Review> findTop(@Param("beginOfParam") LocalDateTime beginOfDay, Pageable pageable);
+
+    default List<Review> findTopOfBeforeOneDay(Pageable pageable) {
+        LocalDateTime beforeOneDay = LocalDateTime.now().minusDays(1);
+        return findTop(beforeOneDay, pageable);
+    }
+
+    default List<Review> findTopOfToday(Pageable pageable) {
+        LocalDate today = LocalDate.now();
+        LocalDateTime beginOfToday = today.atStartOfDay();
+        // Todo : 일단 beginOfDay도 1년치로 바꿔둠
+//        return findTop(beginOfToday, pageable);
+        return findTop(LocalDateTime.now().minusYears(1), pageable);
+    }
+
+    default List<Review> findTopOfTheWeek(Pageable pageable) {
+        LocalDate weekBeforeToday = LocalDate.now().minusWeeks(1);
+        LocalDateTime beginOfWeekBeforeToday = weekBeforeToday.atStartOfDay();
+        return findTop(beginOfWeekBeforeToday, pageable);
+    }
 }
--- udog_server/src/main/java/com/mainproject/udog_server/styleLike/StyleLikeService.java
+++ udog_server/src/main/java/com/mainproject/udog_server/styleLike/StyleLikeService.java
@@ -5,18 +5,18 @@ import com.mainproject.udog_server.hairshopLike.*;
 import com.mainproject.udog_server.member.Member;
 import com.mainproject.udog_server.review.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.*;
 
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class StyleLikeService {
+    private static final int AMOUNT_OF_TOP = 3;
     private final StyleLikeRepository styleLikeRepository;
-    private final ReviewRepository reviewRepository;
     private final ReviewService reviewService;
-
-
 
     public StyleLike createLike(long reviewId, Member member) {
         Review foundReview = reviewService.findReview(reviewId);
@@ -27,6 +27,11 @@ public class StyleLikeService {
         styleLike.setReview(foundReview);
 
         return  styleLikeRepository.save(styleLike);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Review> findTopStyleLikeByDay(){
+        return styleLikeRepository.findTopOfBeforeOneDay(PageRequest.of(0,AMOUNT_OF_TOP));
     }
 
     public void deleteLike(long styleLikeId) {
