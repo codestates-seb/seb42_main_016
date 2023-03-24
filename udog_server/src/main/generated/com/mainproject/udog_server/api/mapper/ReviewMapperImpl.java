3ef027198e917a77b5ab479c03ea6190e95f155e
kim changha <ckdgk0366@gmail.com>
2023-03-25 02:25:22 +0900

    [수정] GET memberReview의 hairShopId, GET hairShopReview의 memberId 나오게 하기 (2023.03.25 김창하)

--- udog_server/src/main/generated/com/mainproject/udog_server/api/mapper/ReviewMapperImpl.java
+++ udog_server/src/main/generated/com/mainproject/udog_server/api/mapper/ReviewMapperImpl.java
@@ -2,18 +2,14 @@ package com.mainproject.udog_server.api.mapper;
 
 import com.mainproject.udog_server.api.dto.ReviewDto.Patch;
 import com.mainproject.udog_server.api.dto.ReviewDto.Response;
-import com.mainproject.udog_server.api.dto.ReviewDto.hairShopReviewsResponse;
-import com.mainproject.udog_server.api.dto.ReviewDto.memberReviewsResponse;
 import com.mainproject.udog_server.review.Review;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-24T19:10:47+0900",
+    date = "2023-03-25T02:19:22+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
 )
 @Component
@@ -55,77 +51,5 @@ public class ReviewMapperImpl implements ReviewMapper {
         Response response = new Response( reviewId, reviewImage, reviewText, createdAt, modifiedAt );
 
         return response;
-    }
-
-    @Override
-    public List<hairShopReviewsResponse> hairShopReviewsToReviewResponseDto(List<Review> reviews) {
-        if ( reviews == null ) {
-            return null;
-        }
-
-        List<hairShopReviewsResponse> list = new ArrayList<hairShopReviewsResponse>( reviews.size() );
-        for ( Review review : reviews ) {
-            list.add( reviewTohairShopReviewsResponse( review ) );
-        }
-
-        return list;
-    }
-
-    @Override
-    public List<memberReviewsResponse> memberReviewsToReviewResponseDto(List<Review> reviews) {
-        if ( reviews == null ) {
-            return null;
-        }
-
-        List<memberReviewsResponse> list = new ArrayList<memberReviewsResponse>( reviews.size() );
-        for ( Review review : reviews ) {
-            list.add( reviewTomemberReviewsResponse( review ) );
-        }
-
-        return list;
-    }
-
-    protected hairShopReviewsResponse reviewTohairShopReviewsResponse(Review review) {
-        if ( review == null ) {
-            return null;
-        }
-
-        Long reviewId = null;
-        String reviewImage = null;
-        String reviewText = null;
-        LocalDateTime createdAt = null;
-
-        reviewId = review.getReviewId();
-        reviewImage = review.getReviewImage();
-        reviewText = review.getReviewText();
-        createdAt = review.getCreatedAt();
-
-        Long memberId = null;
-
-        hairShopReviewsResponse hairShopReviewsResponse = new hairShopReviewsResponse( reviewId, memberId, reviewImage, reviewText, createdAt );
-
-        return hairShopReviewsResponse;
-    }
-
-    protected memberReviewsResponse reviewTomemberReviewsResponse(Review review) {
-        if ( review == null ) {
-            return null;
-        }
-
-        Long reviewId = null;
-        String reviewImage = null;
-        String reviewText = null;
-        LocalDateTime createdAt = null;
-
-        reviewId = review.getReviewId();
-        reviewImage = review.getReviewImage();
-        reviewText = review.getReviewText();
-        createdAt = review.getCreatedAt();
-
-        Long hairShopId = null;
-
-        memberReviewsResponse memberReviewsResponse = new memberReviewsResponse( reviewId, hairShopId, reviewImage, reviewText, createdAt );
-
-        return memberReviewsResponse;
     }
 }
--- udog_server/src/main/java/com/mainproject/udog_server/api/mapper/ReviewMapper.java
+++ udog_server/src/main/java/com/mainproject/udog_server/api/mapper/ReviewMapper.java
@@ -6,6 +6,8 @@ import com.mainproject.udog_server.reservation.*;
 import com.mainproject.udog_server.review.Review;
 import org.mapstruct.Mapper;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
@@ -26,6 +28,75 @@ public interface ReviewMapper {
     }
     Review reviewPatchDtoToReview(ReviewDto.Patch patchDto);
     ReviewDto.Response reviewToReviewResponseDto(Review review);
-    List<ReviewDto.hairShopReviewsResponse> hairShopReviewsToReviewResponseDto(List<Review> reviews);
-    List<ReviewDto.memberReviewsResponse> memberReviewsToReviewResponseDto(List<Review> reviews);
+//    List<ReviewDto.hairShopReviewsResponse> hairShopReviewsToReviewResponseDto(List<Review> reviews);
+    default List<ReviewDto.hairShopReviewsResponse> hairShopReviewsToReviewResponseDto(List<Review> reviews) {
+        if ( reviews == null ) {
+            return null;
+        }
+
+        List<ReviewDto.hairShopReviewsResponse> list = new ArrayList<ReviewDto.hairShopReviewsResponse>( reviews.size() );
+        for ( Review review : reviews ) {
+            list.add( reviewToHairShopReviewsResponse( review ) );
+        }
+
+        return list;
+    }
+
+    default ReviewDto.hairShopReviewsResponse reviewToHairShopReviewsResponse(Review review) {
+        if ( review == null ) {
+            return null;
+        }
+
+        Long reviewId = null;
+        String reviewImage = null;
+        String reviewText = null;
+        LocalDateTime createdAt = null;
+        Long memberId = null;
+
+        reviewId = review.getReviewId();
+        reviewImage = review.getReviewImage();
+        reviewText = review.getReviewText();
+        createdAt = review.getCreatedAt();
+        memberId = review.getMember().getMemberId();
+
+        ReviewDto.hairShopReviewsResponse hairShopReviewsResponse = new ReviewDto.hairShopReviewsResponse( reviewId, memberId, reviewImage, reviewText, createdAt );
+
+        return hairShopReviewsResponse;
+    }
+//    List<ReviewDto.memberReviewsResponse> memberReviewsToReviewResponseDto(List<Review> reviews);
+
+    default List<ReviewDto.memberReviewsResponse> memberReviewsToReviewResponseDto(List<Review> reviews) {
+        if ( reviews == null ) {
+            return null;
+        }
+
+        List<ReviewDto.memberReviewsResponse> list = new ArrayList<ReviewDto.memberReviewsResponse>( reviews.size() );
+        for ( Review review : reviews ) {
+            list.add( reviewTomemberReviewsResponse( review ) );
+        }
+
+        return list;
+    }
+
+    default ReviewDto.memberReviewsResponse reviewTomemberReviewsResponse(Review review) {
+        if ( review == null ) {
+            return null;
+        }
+
+        Long reviewId = null;
+        String reviewImage = null;
+        String reviewText = null;
+        LocalDateTime createdAt = null;
+        Long hairShopId = null;
+
+        reviewId = review.getReviewId();
+        reviewImage = review.getReviewImage();
+        reviewText = review.getReviewText();
+        createdAt = review.getCreatedAt();
+        hairShopId = review.getHairShop().getHairShopId();
+
+        ReviewDto.memberReviewsResponse memberReviewsResponse = new ReviewDto.memberReviewsResponse( reviewId, hairShopId, reviewImage, reviewText, createdAt );
+
+        return memberReviewsResponse;
+    }
 }
