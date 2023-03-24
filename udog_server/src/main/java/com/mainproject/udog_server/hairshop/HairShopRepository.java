d108f0ca4b163b1ddac0eb367cbd13ad11b1e4a8
sayole <sylee8191@gmail.com>
2023-03-24 15:53:01 +0900

    [수정] 헤어샵 top10 (2023.03.24 이상윤)

--- udog_server/src/main/java/com/mainproject/udog_server/hairshop/HairShopRepository.java
+++ udog_server/src/main/java/com/mainproject/udog_server/hairshop/HairShopRepository.java
@@ -1,5 +1,6 @@
 package com.mainproject.udog_server.hairshop;
 
+import com.mainproject.udog_server.review.*;
 import org.springframework.data.domain.*;
 import org.springframework.data.geo.*;
 import org.springframework.data.jpa.repository.*;
@@ -9,27 +10,8 @@ import java.time.*;
 import java.util.*;
 
 public interface HairShopRepository extends JpaRepository<HairShop, Long> {
-    //Todo : hs.created는 데이터 생성날짜고 hs.hairShopLikes.createdAt이 되어야함
-    //       먼저 hairShopLike 엔티티에 createdAt 만들어둠
-    //Todo : StyleBook도 비슷하게 적용시켜야함
-
-//    @Query("SELECT hs FROM HairShop hs WHERE hs.hairShopLikes.createdAt > :beginOfParam ORDER BY hs.hairShopLikes.size DESC")
-    @Query("SELECT hs FROM HairShop hs ORDER BY hs.hairShopLikes.size DESC")
-    Page<HairShop> findTop(@Param("beginOfParam") LocalDateTime beginOfDay, Pageable pageable);
-
-    default Page<HairShop> findTopOfToday(Pageable pageable) {
-        LocalDate today = LocalDate.now();
-        LocalDateTime beginOfToday = today.atStartOfDay();
-        // Todo : 일단 beginOfDay도 1년치로 바꿔둠
-//        return findTop(beginOfToday, pageable);
-        return findTop(LocalDateTime.now().minusYears(1), pageable);
-    }
-
-    default Page<HairShop> findTopOfTheWeek(Pageable pageable) {
-        LocalDate weekBeforeToday = LocalDate.now().minusWeeks(1);
-        LocalDateTime beginOfWeekBeforeToday = weekBeforeToday.atStartOfDay();
-        return findTop(beginOfWeekBeforeToday, pageable);
-    }
+    @Query("SELECT hs FROM HairShop hs WHERE hs IN (:topList) ORDER BY hs.hairShopLikes.size DESC")
+    Page<HairShop> findTop(@Param("topList")List<HairShop> topList, Pageable pageable);
 
 //    @Query(value = "SELECT *" +
 ////            "ST_Distance_Sphere(Point(:x,:y),POINT(hs.longitude, hs.latitude)) AS hs.distance"+
--- udog_server/src/main/java/com/mainproject/udog_server/hairshopLike/HairShopLikeRepository.java
+++ udog_server/src/main/java/com/mainproject/udog_server/hairshopLike/HairShopLikeRepository.java
@@ -2,13 +2,39 @@ package com.mainproject.udog_server.hairshopLike;
 
 import com.mainproject.udog_server.hairshop.HairShop;
 import com.mainproject.udog_server.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.mainproject.udog_server.review.*;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.*;
 
-import java.util.Optional;
+import java.time.*;
+import java.util.*;
 
 public interface HairShopLikeRepository extends JpaRepository<HairShopLike, Long> {
     Optional<HairShopLike> findByHairShopLikeId(Long hairShopId);
     int countByHairShopHairShopId(Long hairShopId);
 
     Optional<HairShopLike> findByMemberAndHairShop(Member memberId, HairShop hairShopId);
+
+    @Query("SELECT hsl.hairShop, count(hsl.hairShop) FROM HairShopLike hsl WHERE hsl.createdAt > :beginOfParam GROUP BY hsl.hairShop ORDER BY count(hsl.hairShop) DESC")
+    List<HairShop> findTop(@Param("beginOfParam") LocalDateTime beginOfDay, Pageable pageable);
+
+    default List<HairShop> findTopOfBeforeOneDay(Pageable pageable) {
+        LocalDateTime beforeOneDay = LocalDateTime.now().minusDays(1);
+        return findTop(beforeOneDay, pageable);
+    }
+
+    default List<HairShop> findTopOfToday(Pageable pageable) {
+        LocalDate today = LocalDate.now();
+        LocalDateTime beginOfToday = today.atStartOfDay();
+        // Todo : 일단 beginOfDay도 1년치로 바꿔둠
+//        return findTop(beginOfToday, pageable);
+        return findTop(LocalDateTime.now().minusYears(1), pageable);
+    }
+
+    default List<HairShop> findTopOfTheWeek(Pageable pageable) {
+        LocalDate weekBeforeToday = LocalDate.now().minusWeeks(1);
+        LocalDateTime beginOfWeekBeforeToday = weekBeforeToday.atStartOfDay();
+        return findTop(beginOfWeekBeforeToday, pageable);
+    }
 }
