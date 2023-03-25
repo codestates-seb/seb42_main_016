616b4d8464ce3194634d93a7911e7d2f60471918
soohyuneee <tlstngus95@naver.com>
2023-03-25 21:45:00 +0900

    [수정] 준비중 수정 (2023.03.25 신수현)

--- client/src/pages/Prepare.js
+++ client/src/pages/Prepare.js
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 import { IoConstructOutline } from 'react-icons/io5';
+import { useNavigate } from 'react-router-dom';
+import { HOME } from '../modules/routes';
+
 function Prepare() {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <Content>
         <IoConstructOutline className="icon" />
         <h2>서비스 준비중입니다</h2>
+        <GoHome role="presentation" onClick={() => navigate(HOME)}>
+          홈으로 가기
+        </GoHome>
       </Content>
     </Container>
   );
@@ -17,17 +25,27 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 200px;
+  height: 100vh;
 `;
 
 const Content = styled.div`
-  margin: 100px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  color: #333;
   .icon {
     width: 150px;
     height: 150px;
+  }
+`;
+
+const GoHome = styled.div`
+  margin-top: 0;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+  :hover {
+    opacity: 0.5;
   }
 `;
