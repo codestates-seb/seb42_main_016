4fa4b0edf17ad233d72eb0cb840377e7509d04e7
soohyuneee <tlstngus95@naver.com>
2023-03-25 23:58:09 +0900

    [수정] 로딩 컴포넌트 수정 (2023.03.25 신수현)

--- client/src/pages/Stylebook.js
+++ client/src/pages/Stylebook.js
@@ -5,11 +5,12 @@ import { STYLEBOOK_ENDPOINT } from '../modules/endpoints';
 import useInfiniteScroll from '../hooks/useInfiniteScroll';
 import ScrollTopButton from '../components/ScrollTopButton';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 
 function Stylebook() {
   const PER_PAGE = 9;
 
-  const { data, handleScroll } = useInfiniteScroll(STYLEBOOK_ENDPOINT, PER_PAGE);
+  const { data, handleScroll, loading } = useInfiniteScroll(STYLEBOOK_ENDPOINT, PER_PAGE);
 
   const scrollAreaRef = useRef(null);
   const [showButton, setShowButton] = useState(false);
@@ -29,6 +30,7 @@ function Stylebook() {
       <S.StylebookWrap>
         <StylebookList data={data} />
         {showButton && <ScrollTopButton area={scrollAreaRef} />}
+        {loading && <Loading />}
       </S.StylebookWrap>
     </S.StyleScrollArea>
   );
