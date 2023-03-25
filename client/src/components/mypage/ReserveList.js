86f77b145bb70751d18c205b4c5cb3347a4d0333
soohyuneee <tlstngus95@naver.com>
2023-03-26 03:46:41 +0900

    [추가] empty 컴포넌트 연결 (2023.03.26 신수현)

--- client/src/components/mypage/ReserveList.js
+++ client/src/components/mypage/ReserveList.js
@@ -2,6 +2,7 @@ import useFetch from '../../hooks/useFetch';
 import { RESERVATION_ENDPOINT } from '../../modules/endpoints';
 import * as S from '../style/MyPageStyle';
 import ReserveItem from './ReserveItem';
+import Empty from '../Empty';
 
 export default function ReserveList() {
   const reserve = useFetch(`${RESERVATION_ENDPOINT}?page=${1}&size=${10}`)['data'];
@@ -13,11 +14,13 @@ export default function ReserveList() {
           <S.TitleBox>
             <S.Title>예약 내역</S.Title>
           </S.TitleBox>
-          {reserve
-            ? reserve.map((reserve) => {
-                return <ReserveItem key={reserve.reservationId} reserve={reserve} />;
-              })
-            : null}
+          {reserve?.length ? (
+            reserve.map((reserve) => {
+              return <ReserveItem key={reserve.reservationId} reserve={reserve} />;
+            })
+          ) : (
+            <Empty text={'예약 내역이 없습니다.'} />
+          )}
         </S.Content>
       </S.ContentBox>
     </S.Container>
