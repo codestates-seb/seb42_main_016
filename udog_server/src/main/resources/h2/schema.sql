58225dcc7316eec8497e626590f96e4e9b19aee4
sayole <sylee8191@gmail.com>
2023-03-28 04:35:13 +0900

    [수정] S3 재설정 및 db 초기화(2023.03.28 이상윤)

--- udog_server/src/main/resources/h2/schema.sql
+++ udog_server/src/main/resources/h2/schema.sql
@@ -10,11 +10,6 @@ drop table if exists hair_shop CASCADE;
         primary key (hair_shop_id)
     );
 
---CREATE TABLE test_entity (
---  id INT PRIMARY KEY,
---  name VARCHAR(255),
---  addr VARCHAR(255)
---);
 --
 --LOAD DATA LOCAL INFILE '/Users/sangyoonlee/Downloads/DistrictOffice.csv'
 --INTO TABLE district_office
@@ -32,3 +27,10 @@ drop table if exists hair_shop CASCADE;
 --LINES TERMINATED BY '\n'
 --IGNORE 1 ROWS
 --(id, addr);
+
+--SELECT kakao_api_id, COUNT(kakao_api_id)
+--FROM hair_shop
+--GROUP BY COLUMN_NAME
+--HAVING COUNT(COLUMN_NAME) > 1 ;
+--
+--UPDATE hair_shop AS t1 INNER JOIN hair_shop_cp AS t2 ON t1.kakao_api_id = t2.kakao_api_id SET t1.hair_shop_image = t2.hair_shop_image;
