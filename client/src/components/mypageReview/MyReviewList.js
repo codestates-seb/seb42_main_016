e76783311c70307d0a6947bc13a393b9f4df79fc
blueberryade <blueberryade7@gmail.com>
2023-03-27 15:38:10 +0900

    [추가]작성한 리뷰 페이지네이션 추가(2023.03.27 조정인)

--- client/src/components/mypageReview/MyReviewList.js
+++ client/src/components/mypageReview/MyReviewList.js
@@ -1,17 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as S from '../style/MyPageStyle';
 import ReviewItem from './ReviewItem';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectReviews, fetchReviews } from '../../modules/redux/reviewsSlice';
+import { selectReviews, fetchReviews, selectPageInfo } from '../../modules/redux/reviewsSlice';
 import Empty from '../Empty';
+import Pagenation from '../mypage/PagenationR';
 
 export default function MyReviewList() {
   const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
   const reviews = useSelector(selectReviews);
+  const pageInfo = useSelector(selectPageInfo);
+  const totalElements = pageInfo.totalElements;
+  const minLen = 9;
 
   useEffect(() => {
-    dispatch(fetchReviews());
-  }, [dispatch]);
+    dispatch(fetchReviews(currentPage));
+  }, [dispatch, currentPage]);
 
   return (
     <S.Container>
@@ -21,6 +26,15 @@ export default function MyReviewList() {
         })
       ) : (
         <Empty text={'작성한 리뷰가 없습니다.'} />
+      )}
+      {totalElements > minLen ? (
+        <Pagenation
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalElements={totalElements}
+        />
+      ) : (
+        ''
       )}
     </S.Container>
   );
